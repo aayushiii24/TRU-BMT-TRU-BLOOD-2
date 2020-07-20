1712cd6cad7ce3828c13a5515217dcddabdcfcd9
scdi <scdi@me.com>
2020-07-20 15:24:59 -0400

    adding signature page

--- TRU-Pain/ConsentTask.swift
+++ TRU-Pain/ConsentTask.swift
@@ -11,11 +11,21 @@ import ResearchKit
 
 public var ConsentTask: ORKOrderedTask {
   
-  var steps = [ORKStep]()
+    var steps = [ORKStep]()
   
-  //TODO: Add VisualConsentStep
+    var consentDocument = ConsentDocument
+    let visualConsentStep = ORKVisualConsentStep(identifier: "VisualConsentStep", document: consentDocument)
+    steps += [visualConsentStep]
+
   
-  //TODO: Add ConsentReviewStep
-  
-  return ORKOrderedTask(identifier: "ConsentTask", steps: steps)
+    let signature = consentDocument.signatures!.first!
+
+    let reviewConsentStep = ORKConsentReviewStep(identifier: "ConsentReviewStep", signature: signature, in: consentDocument)
+
+    reviewConsentStep.text = "Review Consent!"
+    reviewConsentStep.reasonForConsent = "Consent to join study"
+
+    steps += [reviewConsentStep]
+    return ORKOrderedTask(identifier: "ConsentTask", steps: steps)
+    
 }
